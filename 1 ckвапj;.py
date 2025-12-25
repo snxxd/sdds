64589e44db9e054ca817c421133a4b2cb75d1e6a
vaskovvasilij336-afk <vaskovvasilij336@gmail.com>
2025-12-25 14:17:25 +0500

    Add files via upload

---
+++ 1 ckвапj;.py
@@ -0,0 +1,10 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> KeyboardInterrupt
+>>> values = [10, 5, 0, 2]
+... 
+... for v in values:
+...     if v == 0:
+...         print("Ошибка: деление на ноль")
+...     else:
+...         print(100 / v)
